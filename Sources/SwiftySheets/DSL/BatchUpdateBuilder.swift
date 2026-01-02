f2a9da01efed135f527e6152620190dd7659bed5
Vinay Jain <vinay.jn7@gmail.com>
2026-01-03 01:30:40 +0530

    feat: Introduce BatchUpdateBuilder DSL

---
+++ Sources/SwiftySheets/DSL/BatchUpdateBuilder.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+@resultBuilder
+public struct BatchUpdateBuilder {
+    public static func buildBlock(_ components: BatchUpdateRequest.Request...) -> [BatchUpdateRequest.Request] {
+        components
+    }
+    
+    public static func buildBlock(_ components: [BatchUpdateRequest.Request]...) -> [BatchUpdateRequest.Request] {
+        components.flatMap { $0 }
+    }
+    
+    public static func buildEither(first component: [BatchUpdateRequest.Request]) -> [BatchUpdateRequest.Request] {
+        component
+    }
+    
+    public static func buildEither(second component: [BatchUpdateRequest.Request]) -> [BatchUpdateRequest.Request] {
+        component
+    }
+    
+    public static func buildOptional(_ component: [BatchUpdateRequest.Request]?) -> [BatchUpdateRequest.Request] {
+        component ?? []
+    }
+    
+    // Support for single element expressions
+    public static func buildExpression(_ expression: BatchUpdateRequest.Request) -> [BatchUpdateRequest.Request] {
+        [expression]
+    }
+    
+    public static func buildExpression(_ expression: [BatchUpdateRequest.Request]) -> [BatchUpdateRequest.Request] {
+        expression
+    }
+}
---
+++ Sources/SwiftySheets/DSL/DSLHelpers.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+// These helpers create Request objects easily
+
+public func AddSheet(_ title: String) -> BatchUpdateRequest.Request {
+    .addSheet(AddSheetRequest(properties: Sheet.Draft(title: title, gridProperties: Sheet.GridProperties(rowCount: 1000, columnCount: 26))))
+}
+
+public func DeleteSheet(id: Int) -> BatchUpdateRequest.Request {
+    .deleteSheet(DeleteSheetRequest(sheetId: id))
+}
+
+// More helpers can be added for UpdateCells etc.
