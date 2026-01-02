6ca53712e7858059f6efc79b03833cf969f9ab75
Vinay Jain <vinay.jn7@gmail.com>
2026-01-03 01:30:27 +0530

    chore: Update Package.swift for Swift 6.2 and new targets

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.2
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "SwiftySheets",
@@ -12,19 +13,43 @@ let package = Package(
             name: "SwiftySheets",
             targets: ["SwiftySheets"]
         ),
+        .executable(
+            name: "SwiftySheetsDemo",
+            targets: ["SwiftySheetsDemo"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.0")
+        .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
+        // Core Library
         .target(
             name: "SwiftySheets",
             dependencies: [
-                .product(name: "OAuth2", package: "google-auth-library-swift"),                
+                .product(name: "OAuth2", package: "google-auth-library-swift"),
+                "SwiftySheetsMacros"
             ]
         ),
+        // Macros Implementation
+        .macro(
+            name: "SwiftySheetsMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]
+        ),
+        // Tests
         .testTarget(
             name: "SwiftySheetsTests",
+            dependencies: [
+                "SwiftySheets",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
+        ),
+        // Demo
+        .executableTarget(
+            name: "SwiftySheetsDemo",
             dependencies: ["SwiftySheets"]
         ),
     ]
