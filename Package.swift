76d9acab5a5ec2047f412cf4d72a2e2290176990
Vinay Jain <vinay.jn7@gmail.com>
2026-01-11 18:04:56 +0530

    Fix swift syntax versions

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.0"),
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0"..<"603.0.0"),
     ],
     targets: [
         // Core Library
